b83872a4661d32bb6e55707da652ba439adb4e15
杜苏超 <dusc@sondon.net>
2018-03-08 18:11:30 +0800

    calendar

--- src/pages/calendar/calendar.vue
+++ src/pages/calendar/calendar.vue
@@ -25,27 +25,117 @@ export default {
     data() {
         return {
             active: false,
+            time: '24:00:00',
+            startTime: 0,
+            endTime: 0,
         };
     },
     created() {},
     mounted() {
+        let that = this;
         $('#calendar').fullCalendar({
             header: {
                 left: 'prev,next today',
                 center: 'title',
-                right: 'month,agendaWeek,agendaDay'
+                right: 'agendaDay,agendaWeek,month'
             },
             firstDay: 1,
+            // businessHours: [{
+            //     dow: [1, 2, 3, 4, 5],
+            //     start: '09:30',
+            //     end: '18:00'
+            // }],
+            timeFormat: 'HH:mm',
+            slotLabelFormat: 'HH:mm',
+            slotLabelInterval: '00:30:00',
+            defaultTimedEventDuration: '00:30:00',
+            forceEventDuration: true,
+            nowIndicator: true,
             allDaySlot: false,
             defaultView: 'agendaDay', // 默认视图,
             minTime: '07:00:00',
             maxTime: '24:00:00',
-  
+            height: 'auto',
+            fixedWeekCount: false,
+            showNonCurrentDates: false,
+            slotEventOverlap: false,
+            locale: 'zh-cn',
+            navLinks: true,
             views: {
                 agendaDay: {
                     // titleFormat: 'MMMM D YYYY'
+                    selectable: true,
+                    selectHelper: true,
+                    selectLongPressDelay: 500
                 }
             },
+
+            selectOverlap: false,
+            select: function(start, end) {
+                // var title = prompt('填写你的记录的:');
+                // var eventData;
+                // if (title) {
+                //     eventData = {
+                //         title: title,
+                //         start: start,
+                //         end: end
+                //     };
+                //     $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+                // }
+                // $('#calendar').fullCalendar('unselect');
+            },
+
+            editable: true,
+            eventOverlap: false,
+            eventDragStart: function(event) {
+                that.startTime = event.start.unix();
+                that.endTime = event.end.unix();
+            },
+            eventDrop: function(event, delta, revertFunc) {
+                let start = event.start.unix();
+                let end = event.end.unix();
+                if ((start > that.startTime && start < that.endTime) || (end > that.startTime && end < that.endTime)) {
+                    that.startTime = event.start.unix();
+                    that.endTime = event.end.unix();
+                } else {
+                    revertFunc();
+                    $('#calendar').fullCalendar('renderEvent', event, true);
+                }
+
+                let events = $('#calendar').fullCalendar('getEventSources');
+                console.log(events);
+            },
+
+            displayEventTime: true,
+            displayEventEnd: true,
+            eventLimit: 3,
+            events: [
+                {
+                    title: 'hello',
+                    start: '2018-03-08T17:30:00',
+                    end: '2018-03-08T18:00:00',
+                    editable: true
+                },
+                {
+                    title: '工作',
+                    start: '2018-03-08T09:30:00',
+                    end: '2018-03-08T12:00:00',
+                },
+                {
+                    title: '吃饭',
+                    start: '2018-03-08T12:00:00',
+                    end: '2018-03-08T12:30:00',
+                },
+                {
+                    title: '休息',
+                    start: '2018-03-08T12:30:00',
+                    end: '2018-03-08T13:30:00',
+                }
+            ],
+
+            windowResize: function(view) {
+                console.log(view);
+            }
 
             // navLinks: true, // can click day/week names to navigate views
             // selectable: true,
