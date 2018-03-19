4685b8705c19a3ad7130ea7766d4d920d1bfe7ac
杜苏超 <dusc@sondon.net>
2018-03-19 11:18:48 +0800

    路由

---
+++ src/pages/home.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <Header></Header>
+        <Sidebar @is-bespread="isBespread"></Sidebar>
+        <router-view class="tpl-content-wrapper" :class="{active}"></router-view>
+    </div>
+</template>
+
+<script>
+import Header from '@/components/header';
+import Sidebar from '@/components/sidebar';
+
+export default {
+    name: 'Home',
+    components: {
+        Header,
+        Sidebar
+    },
+    data() {
+        return {
+            active: false,
+        };
+    },
+    methods: {
+        isBespread(bool) {
+            this.active = bool;
+        }
+    }
+};
+</script>
